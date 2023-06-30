01121a559beb83ba641d7e5423ef3bd29e373470
槿泽 <1791209985@qq.com>
2023-06-30 20:35:25 +0800

    添加http测试文件

--- member/src/main/java/com/jinze/train/controller/HelloController.java
+++ member/src/main/java/com/jinze/train/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.jinze.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * TODO
+ * hello测试类
  * @author jinze
  * @version 1.0
  */
 @RestController
+@RequestMapping("member")
 public class HelloController {
 
     @GetMapping("/hello")
