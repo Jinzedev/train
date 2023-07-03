a1490a051eb614e2b6a05cde47637d5972c576d7
槿泽 <1791209985@qq.com>
2023-07-03 19:26:27 +0800

    添加gateway网关模块，用于请求转发

---
+++ gateway/src/main/java/com/jinze/train/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.jinze.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 网关启动类
+ * @author jinze
+ * @version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
